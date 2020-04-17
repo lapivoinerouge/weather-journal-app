62bbfc68e1d93d4e7a430d54d8ba33a539486e43
lapivoinerouge <wioladys@gmail.com>
2020-04-17 16:51:30 +0200

    get/post routes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, function() {
+    console.log('server running on port:' + port);
+});
+
+//GET route
+app.get('/', function(req, res) {
+    res.send(projectData);
+});
+
+//POST route
+app.post('/', function(req, res) {
+    let newData = req.body;
+    let newEntry = {
+        temperature: newData.temperature,
+        date: newData.date,
+        userResponse: newData.userResponse
+    };
+    projectData.push(newEntry);
+});
